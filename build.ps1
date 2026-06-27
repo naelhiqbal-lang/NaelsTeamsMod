95020766beeb0ac3d57cdbd839a0b80178f15e96
nael <naelhiqbal-lang@users.noreply.github.com>
2026-06-27 16:20:43 +0100

    Flatten repository layout

--- build.ps1
+++ build.ps1
@@ -3,15 +3,11 @@ $ErrorActionPreference = 'Stop'
 $csc = 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe'
 $managed = 'C:\Program Files (x86)\Steam\steamapps\common\Super Battle Golf\Super Battle Golf_Data\Managed'
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
-$workspace = Split-Path -Parent $root
-$outDir = Join-Path $root 'dist'
 
-New-Item -ItemType Directory -Force -Path $outDir | Out-Null
-
-& $csc /nologo /target:library /optimize+ /out:"$outDir\NaelsTeamsMod-0.4.17.dll" `
+& $csc /nologo /target:library /optimize+ /out:"$root\NaelsTeamsMod-0.4.17.dll" `
   /reference:"$managed\netstandard.dll" `
-  /reference:"$workspace\core\BepInEx.dll" `
-  /reference:"$workspace\core\0Harmony.dll" `
+  /reference:"$root\core\BepInEx.dll" `
+  /reference:"$root\core\0Harmony.dll" `
   /reference:"$managed\GameAssembly.dll" `
   /reference:"$managed\SharedAssembly.dll" `
   /reference:"$managed\Mirror.dll" `
@@ -24,4 +20,4 @@ New-Item -ItemType Directory -Force -Path $outDir | Out-Null
   /reference:"$managed\UnityEngine.UIModule.dll" `
   /reference:"$managed\UnityEngine.TextRenderingModule.dll" `
   /reference:"$managed\Unity.TextMeshPro.dll" `
-  "$root\ModernTeamMode.cs"
+  "$root\NaelsTeamsMod.cs"
